65ee0cd2e3e6285151d8e3b011213504389cc454
sparkeUnlimited <rmaxwell@sparkeunlimited.ca>
2025-07-04 13:22:46 -0400

    Changed Header data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,11 @@ import "/styles/globals.css";
 import "/styles/calendar.css";
 
 export const metadata = {
-  title: "Spark-E PM App",
+  title: "Estimating App - Spark-E",
   description:
-    "Delivering projects on time and within budget. Providing sustainable electrical services for residential and commercial customers.",
+    "Providing timely and complete estimates. Sustainable electrical services for residential and commercial customers.",
   keywords:
-    "electrician, contractor, EV chargers, panel upgrades, energy-efficient, fire alarm, commercial, residential",
+    "electrician, contractor, estimating, estimate, EV chargers, panel upgrades, energy-efficient, fire alarm, commercial, residential",
 };
 
 const RootLayout = ({ children }) => (
