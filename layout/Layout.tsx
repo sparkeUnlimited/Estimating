e2b7bd6de601922e7b4c8eac77988a26189ee123
sparkeUnlimited <rmaxwell@sparkeunlimited.ca>
2025-07-05 12:39:36 -0400

    Updated content layout to display properly

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({ title = "Estimating Tool", children }: LayoutProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      {/* Black Overlay 
+      {/* Black Overlay */}
       <Box
         sx={{
           backgroundColor: "black",
@@ -29,7 +29,7 @@ const Layout = ({ title = "Estimating Tool", children }: LayoutProps) => {
         }}
         className="main"
       >
-        Radial Gradient Background 
+        Radial Gradient Background
         <Box
           sx={{
             zIndex: 3,
@@ -52,7 +52,7 @@ const Layout = ({ title = "Estimating Tool", children }: LayoutProps) => {
           }}
           className="gradient"
         />
-      </Box> */}
+      </Box>
 
       {/* Main Content Container */}
       <Container
@@ -67,16 +67,6 @@ const Layout = ({ title = "Estimating Tool", children }: LayoutProps) => {
           px: { xs: 2, sm: 6 },
           mx: "auto",
           minHeight: "100vh",
-          backgroundImage: `
-              radial-gradient(at 27% 37%, hsla(215, 98%, 61%, 1) 0px, transparent 0%),
-              radial-gradient(at 97% 21%, hsla(125, 98%, 72%, 1) 0px, transparent 50%),
-              radial-gradient(at 52% 99%, hsla(354, 98%, 61%, 1) 0px, transparent 50%),
-              radial-gradient(at 10% 29%, hsla(256, 96%, 67%, 1) 0px, transparent 50%),
-              radial-gradient(at 97% 96%, hsla(38, 60%, 74%, 1) 0px, transparent 50%),
-              radial-gradient(at 33% 50%, hsla(222, 67%, 73%, 1) 0px, transparent 50%),
-              radial-gradient(at 79% 53%, hsla(343, 68%, 79%, 1) 0px, transparent 50%)
-            `,
-          filter: "blur(100px) saturate(150%)",
         }}
       >
         <Nav />
